851cdffb8fd05f90a526aaf29af507e5efec9096
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-03-25 11:44:48 -0300

    Ejercicio1Finished

--- Assets/Scripts/DeclaracionDeVariables.cs
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class DeclaracionDeVariables : MonoBehaviour
 {
-    int cantidadAlumnos;
-    float magnitudTerremoto;
-    string nombreAlumno;
-    bool compuHabilitada;
+    int cantidadAlumnos = 24;
+    float magnitudTerremoto = 9.3f;
+    string nombreAlumno = "Agustin";
+    bool compuHabilitada = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(cantidadAlumnos);
+        Debug.Log(magnitudTerremoto);
+        Debug.Log(nombreAlumno);
+        Debug.Log(compuHabilitada);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
