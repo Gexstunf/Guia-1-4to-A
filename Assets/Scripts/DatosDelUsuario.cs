07bf206936dddf77408bd547170d49e3ee342cad
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-03-28 08:08:35 -0300

    Ejercicio 2 semi

---
+++ Assets/Scripts/DatosDelUsuario.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DatosDelUsuario : MonoBehaviour
+{
+    string nombre = "Agustin";
+    int edad = 17;
+    float estatura = 1.83f;
+    bool donante = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mi nombre es " + nombre + ", tengo " + edad + ", mido " + estatura + " metros y es " + donante + " que soy donante de órganos.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
