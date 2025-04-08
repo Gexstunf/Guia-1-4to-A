820cc3b942ae64e91656d364ebf8836d53442b33
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-04-08 12:12:10 -0300

    Examenes

---
+++ Assets/Scripts/Examenes/convercionMoneda.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class convercionMoneda : MonoBehaviour
+{
+    public string moneda;
+    int resultado;
+    public int monto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (monto >= 1000)
+        {
+            if (moneda == "D")
+            {
+                resultado = 4 * monto;
+                Debug.Log(resultado);
+            }
+            else if (moneda == "R")
+            {
+                resultado = 5 * monto;
+                Debug.Log(resultado);
+            }
+            else if (moneda == "E")
+            {
+                resultado = 6 * monto;
+                Debug.Log(resultado);
+            }
+            else
+            {
+                Debug.Log("moenda tiene que ser {E; D; D}");
+            }
+        }else if (monto < 1000)
+        {
+            Debug.Log("monto minimo es 1000");
+            return;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Examenes/deQueOrientacion.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deQueOrientacion : MonoBehaviour
+{
+    public int grado;
+    public string alumno;
+    public string orientacion;
+
+    void Start()
+    {
+        if (string.IsNullOrEmpty(alumno))
+        {
+            Debug.Log("Pon un alumno");
+            return;
+        }
+
+        if (grado < 1 || grado > 5)
+        {
+            Debug.Log("Grado tiene que estar entre 1 y 5");
+            return;
+        }
+
+        if (grado == 1 || grado == 2)
+        {
+            Debug.Log("Tienes que ser de grado 3 o mayor");
+            return;
+        }
+
+        else if (orientacion != "T" && orientacion != "G" && orientacion != "M" && orientacion != "H" && orientacion != "D")
+        {
+            Debug.Log("no se pudo");
+            return;
+        }
+
+        else
+        {
+            Debug.Log("¡Muchas gracias, " + alumno + "!");
+        }
+
+    }
+}
